47dfad0ba777d70e24683c1a8cbd5f26c0079bb4
BOLUWATIFE-DAVID-ADANLA <adanlab4@gmail.com>
2024-12-30 16:19:21 +0100

    animation test and image addition sucessful

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,34 +28,44 @@ class MainView extends StatelessWidget {
   }
 }
 
-class FlameTestGame extends FlameGame {
+class FlameTestGame extends FlameGame with PanDetector {
   late Player _player;
   @override
-  Color backgroundColor() => Colors.blue;
-
+  Color backgroundColor() => Colors.black;
   @override
   FutureOr<void> onLoad() async {
     await super.onLoad();
 
-    add(
-      Player()
-        ..position = size / 2
-        ..width = 50
-        ..height = 100
-        ..anchor = Anchor.center,
-    );
+    debugPrint('image loaded');
+
+    _player = Player();
+
+    add(_player);
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    _player.move(info.delta.global);
   }
 }
 
-class Player extends PositionComponent {
-  static final _paint = Paint()..color = Colors.white;
+class Player extends SpriteAnimationComponent
+    with HasGameReference<FlameTestGame> {
+  Player({Vector2? size, super.anchor = Anchor.center})
+      : super(size: size ?? Vector2(48, 48));
 
   @override
-  void render(Canvas canvas) {
-    canvas.drawRect(size.toRect(), _paint);
+  FutureOr<void> onLoad() async {
+    animation = await game.loadSpriteAnimation(
+        'Space_ghost_right_idle.png',
+        SpriteAnimationData.sequenced(
+            loop: true, amount: 9, stepTime: .2, textureSize: Vector2(48, 48)));
+    position = game.size / 2;
+
+    return super.onLoad();
   }
 
-  // void move(Vector2 img) {
-  //   position.add(img);
-  // }
+  void move(Vector2 img) {
+    position.add(img);
+  }
 }
